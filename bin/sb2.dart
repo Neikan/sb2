cefaffb6a48c24d2978fb26be3d5a9c6c8bc4c73
Neikan <sladkov.e.m@outlook.com>
2022-12-11 16:07:54 +0300

    feat(exercise 8): add code

--- bin/sb2.dart
+++ bin/sb2.dart
@@ -5,26 +5,30 @@ import 'package:sb2/exercise_4.dart';
 import 'package:sb2/exercise_5.dart';
 import 'package:sb2/exercise_6.dart';
 import 'package:sb2/exercise_7.dart';
+import 'package:sb2/exercise_8.dart';
 
 void main(List<String> arguments) {
-  // НОД, НОК, множители
-  executeExercise1();
+  // // НОД, НОК, множители
+  // executeExercise1();
 
-  // Конвертации между системами счисления
-  executeExercise2();
+  // // Конвертации между системами счисления
+  // executeExercise2();
 
-  // Поиск чисел в строке
-  executeExercise3();
+  // // Поиск чисел в строке
+  // executeExercise3();
 
-  // Подсчет слов в списке
-  executeExercise4();
+  // // Подсчет слов в списке
+  // executeExercise4();
 
-  // Подсчет цифр с строке
-  executeExercise5();
+  // // Подсчет цифр с строке
+  // executeExercise5();
 
-  // Расстояние между двумя точками в пространстве
-  executeExercise6();
+  // // Расстояние между двумя точками в пространстве
+  // executeExercise6();
 
-  // Корень степени N
-  executeExercise7();
+  // // Корень степени N
+  // executeExercise7();
+
+  // Список пользователей
+  executeExercise8();
 }
